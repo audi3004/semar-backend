df49d3ac3851f63a91c9912929b70e650f15abf1
audi <ubaidillah30.ud@gmail.com>
2026-08-12 15:54:57 +0700

    update

--- src/config/config.js
+++ src/config/config.js
@@ -2,21 +2,19 @@ require("dotenv").config();
 
 const databaseConfig = {
     username: process.env.DB_USER,
-    password:
-        process.env.DB_PASSWORD ||
-        null,
+    password: process.env.DB_PASSWORD || null,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: Number(
-        process.env.DB_PORT || 3306
-    ),
-    dialect:
-        process.env.DB_DIALECT ||
-        "mariadb",
+    port: Number(process.env.DB_PORT || 3306),
+
+    dialect: process.env.DB_DIALECT || "mariadb",
+
+    dialectOptions: {
+        allowPublicKeyRetrieval: true,
+    },
 
     timezone: "+07:00",
     logging: false,
-
 };
 
 module.exports = {
